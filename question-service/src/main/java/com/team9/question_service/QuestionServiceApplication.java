5b1717f96d2cd74987725920693e583d43bc77b7
Junseo Yoon <58902513+liljuice@users.noreply.github.com>
2025-06-23 10:49:30 +0900

    feat: question-service 모듈 초기화 및 기본 MSA 설정

--- question-service/src/main/java/com/team9/question_service/QuestionServiceApplication.java
+++ question-service/src/main/java/com/team9/question_service/QuestionServiceApplication.java
@@ -2,8 +2,14 @@ package com.team9.question_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient; // import 변경
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableScheduling
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
