b1a40e57ada55ef392fa895ff1f6e43a6002357c
131c2 <bg3189@naver.com>
2025-06-23 12:34:45 +0900

    Fix: Test코드 주석 처리

--- ai-feedback-service/src/test/java/com/team9/ai_feedback_service/AiFeedbackServiceApplicationTests.java
+++ ai-feedback-service/src/test/java/com/team9/ai_feedback_service/AiFeedbackServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AiFeedbackServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
