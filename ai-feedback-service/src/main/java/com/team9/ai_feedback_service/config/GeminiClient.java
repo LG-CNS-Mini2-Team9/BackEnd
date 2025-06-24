4bd75018117eb6688e8afc432f81f4f700a8cac6
131c2 <bg3189@naver.com>
2025-06-25 03:15:55 +0900

    Refactor: 환경변수 적용방식으로 변경

--- ai-feedback-service/src/main/java/com/team9/ai_feedback_service/config/GeminiClient.java
+++ ai-feedback-service/src/main/java/com/team9/ai_feedback_service/config/GeminiClient.java
@@ -2,19 +2,22 @@ package com.team9.ai_feedback_service.config;
 
 import com.google.genai.Client;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Getter
 @Component
 public class GeminiClient {
 
-    @Value("${gemini.api.model:gemini-2.5-flash-001}")
+    @Value("${GEMINI_MODEL:gemini-2.0-flash}")
     private String model;
 
     public final Client client;
 
-    public GeminiClient(@Value("${gemini.api.key}") String apiKey) {
+    public GeminiClient(@Value("${GEMINI_API_KEY}") String apiKey) {
+        log.info("제미나이 모델: {}", model);
         this.client = Client.builder().apiKey(apiKey).build();
     }
 }
