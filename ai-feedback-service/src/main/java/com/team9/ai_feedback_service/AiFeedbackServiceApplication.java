d473afdef910fe5b8ff352344cb423a52ee57c32
seohee-P <p03061180@gmail.com>
2025-06-24 17:20:20 +0900

    feat: 데이터베이스 연결 설정

--- ai-feedback-service/src/main/java/com/team9/ai_feedback_service/AiFeedbackServiceApplication.java
+++ ai-feedback-service/src/main/java/com/team9/ai_feedback_service/AiFeedbackServiceApplication.java
@@ -2,10 +2,9 @@ package com.team9.ai_feedback_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// 임시
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
+@SpringBootApplication()
 public class AiFeedbackServiceApplication {
 
 	public static void main(String[] args) {
