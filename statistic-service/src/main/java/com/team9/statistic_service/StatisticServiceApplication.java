c2368467fbcc176cdeee4d14ecab0c32b8aa2bf9
131c2 <bg3189@naver.com>
2025-06-24 11:46:28 +0900

    refactor: @EnableJpaRepositories 애노테이션 삭제

--- statistic-service/src/main/java/com/team9/statistic_service/StatisticServiceApplication.java
+++ statistic-service/src/main/java/com/team9/statistic_service/StatisticServiceApplication.java
@@ -2,13 +2,12 @@ package com.team9.statistic_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@EnableEurekaClient
-@EnableJpaRepositories
+//@EnableJpaRepositories
 @EnableAsync
 @EnableScheduling
 public class StatisticServiceApplication {
