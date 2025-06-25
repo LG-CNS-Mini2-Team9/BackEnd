afdc7864da96461c9806ea6d2e9b0c507c7c3da9
sky121016 <sky121016@naver.com>
2025-06-25 13:32:01 +0900

    [feat/#43] 좋아요 기능 수정

---
+++ answer-service/src/main/java/com/team9/answer_service/api/internal/InternalLikeController.java
@@ -0,0 +1,28 @@
+package com.team9.answer_service.api.internal;
+
+import com.team9.answer_service.global.response.CustomResponse;
+import com.team9.answer_service.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/internal/likes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class InternalLikeController {
+    private final LikeService likeService;
+
+    // 내가 받은 좋아요 수 (마이페이지용)
+    @GetMapping("/my")
+    public ResponseEntity<CustomResponse<Long>> countMyTotalLikes(@AuthenticationPrincipal UserDetails userDetails){
+        Long count = likeService.countMyAnswerLikes(userDetails);
+        return ResponseEntity.ok(CustomResponse.ok(count));
+    }
+}
